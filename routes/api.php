745e26ae4e54237acdcf35a6dda135944e89c1af
Laravel Daily <info@laraveldaily.com>
2021-05-19 09:15:52 +0000

    QuickAdminPanel automatic commit

--- routes/api.php
+++ routes/api.php
@@ -12,4 +12,15 @@ Route::group(['prefix' => 'v1', 'as' => 'api.', 'namespace' => 'Api\V1\Admin', '
 
     // Booking
     Route::apiResource('bookings', 'BookingApiController');
+
+    // Product Category
+    Route::post('product-categories/media', 'ProductCategoryApiController@storeMedia')->name('product-categories.storeMedia');
+    Route::apiResource('product-categories', 'ProductCategoryApiController');
+
+    // Product Tag
+    Route::apiResource('product-tags', 'ProductTagApiController');
+
+    // Product
+    Route::post('products/media', 'ProductApiController@storeMedia')->name('products.storeMedia');
+    Route::apiResource('products', 'ProductApiController');
 });
